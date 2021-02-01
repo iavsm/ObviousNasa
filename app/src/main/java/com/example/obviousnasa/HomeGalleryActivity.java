db9ede4cdc8fff095ede66d812b5303074b5893f
Vasim Shaikh <vasim@scissroboy.com>
2021-02-01 07:27:25 +0530

    Project created.

---
+++ app/src/main/java/com/example/obviousnasa/HomeGalleryActivity.java
@@ -0,0 +1,14 @@
+package com.example.obviousnasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeGalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_gallery);
+    }
+}
